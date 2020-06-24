cd44cc953af64191e11946fde8fc5684700e241a
AtillaAtalay <supreme6714@gmail.com>
2020-06-24 23:28:28 +0300

    second commit

--- test.java
+++ test.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 public class test{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
     }
 }
